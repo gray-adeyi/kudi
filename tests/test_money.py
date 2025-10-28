0134a55d20e6540ed76a713be3e8882e134bce33
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-10-28 23:41:26 +0100

    Add tests

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from kudi import Money
 from kudi.currency_codes import CurrencyCode
+from kudi.exceptions import InvalidCurrencyAlphaCodeError, CurrencyMismatchError
 
 
 class MoneyTestCase(TestCase):
@@ -14,21 +15,135 @@ class MoneyTestCase(TestCase):
         self.assertEqual(m.amount, -100)
         self.assertEqual(m.currency.code, CurrencyCode.EUR)
 
-    def test_money_raises_error_on_wrong_currency(self): ...
+    def test_money_raises_error_on_wrong_currency(self):
+        with self.assertRaises(InvalidCurrencyAlphaCodeError) as context:
+            Money(100, "BTC")
+        self.assertEqual(
+            context.exception.args[0],
+            "`BTC` is an invalid currency code, please use 3-letter ISO code e.g. `USD`",
+        )
 
-    def test_money_can_detect_same_currency(self): ...
+    def test_money_can_detect_same_currency(self):
+        m = Money(0, "EUR")
+        om = Money(0, "USD")
+        self.assertFalse(
+            m.is_same_currency_with(om),
+            "expected money of difference currencies not to match",
+        )
+        om = Money(0, "EUR")
+        self.assertTrue(
+            m.is_same_currency_with(om), "expected money of similar currencies to match"
+        )
 
-    def test_can_evaluate_money_equality(self): ...
+    def test_can_evaluate_money_equality(self):
+        m = Money(0, "EUR")
+        samples = [
+            {"amount": -1, "expected": False},
+            {"amount": 0, "expected": True},
+            {"amount": 1, "expected": False},
+        ]
+        for sample in samples:
+            amount = sample["amount"]
+            expected = sample["expected"]
+            with self.subTest(
+                f"check equality of money with amount of 0 with money of amount {amount}"
+            ):
+                om = Money(amount, "EUR")
+                self.assertEqual(
+                    m == om,
+                    expected,
+                    f"expected money with amount of 0 to be == money of amount of {amount} to be {expected}",
+                )
 
-    def test_money_equality_on_different_currencies_raises_error(self): ...
+    def test_money_equality_on_different_currencies_raises_error(self):
+        eur = Money(0, "EUR")
+        usd = Money(0, "USD")
+        with self.assertRaises(CurrencyMismatchError) as context:
+            eur == usd
+        self.assertEqual(
+            context.exception.args[0],
+            "operations on monies with different currencies is not allowed",
+        )
 
-    def test_can_evaluate_money_greater_than(self): ...
+    def test_can_evaluate_money_greater_than(self):
+        m = Money(0, "EUR")
+        samples = [
+            {"amount": -1, "expected": True},
+            {"amount": 0, "expected": False},
+            {"amount": 1, "expected": False},
+        ]
+        for sample in samples:
+            amount = sample["amount"]
+            expected = sample["expected"]
+            with self.subTest(
+                f"check greater than operation of money with amount of 0 with money of amount {amount}"
+            ):
+                om = Money(amount, "EUR")
+                self.assertEqual(
+                    m > om,
+                    expected,
+                    f"expected money with amount of 0 to be > money of amount of {amount} to be {expected}",
+                )
 
-    def test_can_evaluate_money_greater_than_or_equal(self): ...
+    def test_can_evaluate_money_greater_than_or_equal(self):
+        m = Money(0, "EUR")
+        samples = [
+            {"amount": -1, "expected": True},
+            {"amount": 0, "expected": True},
+            {"amount": 1, "expected": False},
+        ]
+        for sample in samples:
+            amount = sample["amount"]
+            expected = sample["expected"]
+            with self.subTest(
+                f"check greater than or equal to operation of money with amount of 0 with money of amount {amount}"
+            ):
+                om = Money(amount, "EUR")
+                self.assertEqual(
+                    m >= om,
+                    expected,
+                    f"expected money with amount of 0 to be >= money of amount of {amount} to be {expected}",
+                )
 
-    def test_can_evaluate_money_less_than(self): ...
+    def test_can_evaluate_money_less_than(self):
+        m = Money(0, "EUR")
+        samples = [
+            {"amount": -1, "expected": False},
+            {"amount": 0, "expected": False},
+            {"amount": 1, "expected": True},
+        ]
+        for sample in samples:
+            amount = sample["amount"]
+            expected = sample["expected"]
+            with self.subTest(
+                f"check less than operation of money with amount of 0 with money of amount {amount}"
+            ):
+                om = Money(amount, "EUR")
+                self.assertEqual(
+                    m < om,
+                    expected,
+                    f"expected money with amount of 0 to be < money of amount of {amount} to be {expected}",
+                )
 
-    def test_can_evaluate_money_less_than_or_equal(self): ...
+    def test_can_evaluate_money_less_than_or_equal(self):
+        m = Money(0, "EUR")
+        samples = [
+            {"amount": -1, "expected": False},
+            {"amount": 0, "expected": True},
+            {"amount": 1, "expected": True},
+        ]
+        for sample in samples:
+            amount = sample["amount"]
+            expected = sample["expected"]
+            with self.subTest(
+                f"check less than or equal to operation of money with amount of 0 with money of amount {amount}"
+            ):
+                om = Money(amount, "EUR")
+                self.assertEqual(
+                    m <= om,
+                    expected,
+                    f"expected money with amount of 0 to be <= money of amount of {amount} to be {expected}",
+                )
 
     def test_money_is_zero(self): ...
 
